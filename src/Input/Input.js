28a8fe8510acc74d8a6c3e90737fcbc16d44b6b7
郭威 <dxcweb2017@gmail.com>
2020-06-08 11:39:58 +0800

    no message

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import Block from 'dxc-flex';
 import Container from '../Container/Container';
-
+const isIOS = () =>
+  !!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
 export default class Input extends Component {
   state = {
     value: '',
@@ -25,13 +25,18 @@ export default class Input extends Component {
   getValue = () => {
     return this.props.value || this.state.value;
   };
+  onBlur =() => {
+    if (isIOS()) {
+      window.scrollTo(0, window.scrollY - 1);
+    }
+  }
   render() {
     const { normalize, label, labelWidth, width, maxLength, placeholder, onChange, ...other } = this.props;
     const containerProps = { label, labelWidth, width, ...other };
     const value = this.getValue();
     return (
       <Container {...containerProps}>
-        <input style={{ flex: 1 }} maxLength={maxLength} value={value} onChange={this.onChange} placeholder={placeholder} />
+        <input onBlur={this.onBlur} style={{ flex: 1 }} maxLength={maxLength} value={value} onChange={this.onChange} placeholder={placeholder} />
       </Container>
     );
   }
